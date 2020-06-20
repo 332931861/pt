801060a90eca9d87cf92d1b2fc8c5f516a6e7607
zph <332931861.com>
2020-06-20 17:58:23 +0800

    提交

---
+++ src/main/java/com/haha/pt/utils/DateUtils.java
@@ -0,0 +1,69 @@
+package com.haha.pt.utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class DateUtils {
+
+
+    public static List<Date> getDatesBetweenUsingJava7(Date startDate, Date endDate) {
+        List<Date> datesInRange = new ArrayList<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(startDate);
+
+        Calendar endCalendar = new GregorianCalendar();
+        endCalendar.setTime(endDate);
+        endCalendar.add(Calendar.DATE, +1);// 日期加1(包含结束)
+
+        while (calendar.before(endCalendar)) {
+            Date result = calendar.getTime();
+            datesInRange.add(result);
+            calendar.add(Calendar.DATE, 1);
+        }
+        return datesInRange;
+    }
+
+
+    /**
+     * 获取两个日期之间的所有日期
+     *
+     * @param startTime
+     *            开始日期
+     * @param endTime
+     *            结束日期
+     * @return
+     */
+    public static List<String> getDays(String startTime, String endTime) {
+
+        // 返回的日期集合
+        List<String> days = new ArrayList<String>();
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date start = dateFormat.parse(startTime);
+            Date end = dateFormat.parse(endTime);
+
+            Calendar tempStart = Calendar.getInstance();
+            tempStart.setTime(start);
+
+            Calendar tempEnd = Calendar.getInstance();
+            tempEnd.setTime(end);
+            tempEnd.add(Calendar.DATE, +1);// 日期加1(包含结束)
+            while (tempStart.before(tempEnd)) {
+                days.add(dateFormat.format(tempStart.getTime()));
+                tempStart.add(Calendar.DAY_OF_YEAR, 1);
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return days;
+    }
+
+
+
+
+}
--- src/test/java/com/haha/pt/PtApplicationTests.java
+++ src/test/java/com/haha/pt/PtApplicationTests.java
@@ -3,11 +3,18 @@ package com.haha.pt;
 import com.haha.pt.test.entity.SysUser;
 import com.haha.pt.test.service.SysUserService;
 
+import com.haha.pt.utils.DateUtils;
+import org.elasticsearch.client.ElasticsearchClient;
+import org.elasticsearch.client.transport.TransportClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
 
 @SpringBootTest
 class PtApplicationTests {
@@ -26,4 +33,29 @@ class PtApplicationTests {
             System.out.println(sysUser);
         }
     }
+
+    @Test
+    void testES(){
+        //client.
+
+    }
+
+    @Test
+    void testDate() throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date start = simpleDateFormat.parse("2020-2-28");
+
+        Date end = simpleDateFormat.parse("2020-3-2");
+
+        List<Date> datesBetweenUsingJava7 = DateUtils.getDatesBetweenUsingJava7(start, end);
+        for (Date date : datesBetweenUsingJava7) {
+            System.out.println(simpleDateFormat.format(date));
+        }
+
+
+    }
+
+
+
+
 }
